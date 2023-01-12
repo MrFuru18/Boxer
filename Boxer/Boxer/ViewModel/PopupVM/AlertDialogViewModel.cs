1f2b785c437662b64b0bb541f8c2ef6e91031b42
Wojciech Babiński <mrfuru@op.pl>
2023-01-12 22:46:18 +0100

    create popup view

---
+++ Boxer/Boxer/ViewModel/PopupVM/AlertDialogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Boxer.ViewModel.PopupVM
+{
+    class AlertDialogViewModel
+    {
+        public ICommand OKCommand { get; private set; }
+
+    }
+}
