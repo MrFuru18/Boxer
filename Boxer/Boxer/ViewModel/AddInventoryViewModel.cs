af71bfc951957128d0d58402ae94c2f3b7dc921d
Wojciech Babiński <mrfuru@op.pl>
2023-01-26 15:20:05 +0100

    Update add viewmodels

--- Boxer/Boxer/ViewModel/AddInventoryViewModel.cs
+++ Boxer/Boxer/ViewModel/AddInventoryViewModel.cs
@@ -14,18 +14,82 @@ namespace Boxer.ViewModel
     class AddInventoryViewModel : BaseViewModel
     {
         public string HeaderText { get; set; }
+
+        private Inventory _inventory = new Inventory();
         public ICommand CancelCommand { get; }
         public ICommand AddInventory { get; }
 
+        private string _locationId;
+        public string LocationId
+        {
+            get { return _locationId; }
+            set
+            {
+                _locationId = value;
+                onPropertyChanged(nameof(LocationId));
+
+                if (LocationId != "")
+                    _inventory.location_id = int.Parse(LocationId);
+            }
+        }
+
+        private string _productId;
+        public string ProductId
+        {
+            get { return _productId; }
+            set
+            {
+                _productId = value;
+                onPropertyChanged(nameof(ProductId));
+
+                if (ProductId != "")
+                    _inventory.product_id = int.Parse(ProductId);
+            }
+        }
+
+        private string _quantity;
+        public string Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                onPropertyChanged(nameof(Quantity));
+
+                if (Quantity != "")
+                    _inventory.quantity = int.Parse(Quantity);
+            }
+        }
+
+        private string _remarks;
+        public string Remarks
+        {
+            get { return _remarks; }
+            set
+            {
+                _remarks = value;
+                onPropertyChanged(nameof(Remarks));
+
+                _inventory.remarks = Remarks;
+            }
+        }
         public AddInventoryViewModel(INavigationService navigationService, Inventory inventory)
         {
             CancelCommand = new NavigateCommand(navigationService);
             AddInventory = new NavigateCommand(navigationService);
 
+            _inventory = new Inventory();
+
             HeaderText = "Dodaj Do Stanów";
             if (inventory != null)
             {
                 HeaderText = "Edytuj Stany";
+
+                _inventory = inventory;
+                LocationId = _inventory.location_id.ToString();
+                ProductId = _inventory.product_id.ToString();
+                Quantity = _inventory.quantity.ToString();
+                Remarks = _inventory.remarks;
             }
         }
     }
--- Boxer/Boxer/ViewModel/AddTaskViewModel.cs
+++ Boxer/Boxer/ViewModel/AddTaskViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using ApiLibrary.Model;
 using Boxer.Commands;
+using Boxer.Model;
 using Boxer.Navigation;
 using Boxer.ViewModel.BaseClass;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Boxer.ViewModel
@@ -15,10 +18,65 @@ namespace Boxer.ViewModel
     {
         public string HeaderText { get; set; }
 
+        public bool TypeOrderChosen { get; set; }
+        public bool TypeSupplyChosen { get; set; }
+        public bool TypeRelocationChosen { get; set; }
+
         private Tasks _task = new Tasks();
+
+        public BindingList<RelocationItem> relocation_items { get; set; }
+        private List<RelocationItem> _relocation_items { get; set; }
+        private RelocationItem _relocationItem = new RelocationItem();
+
+        public BindingList<Employee> employees { get; set; }
+        public BindingList<Order> orders { get; set; }
+        public BindingList<Supply> supplies { get; set; }
 
         public ICommand CancelCommand { get; }
         public ICommand AddTask { get; }
+        private ICommand _addItem;
+        public ICommand AddItem
+        {
+            get
+            {
+                return _addItem ?? (_addItem = new RelayCommand((p) =>
+                {
+                    if (_relocationItem != null)
+                    {
+                        _relocation_items.Add(_relocationItem);
+                        refreshRelocationItems();
+
+                        _relocationItem = new RelocationItem();
+                        _relocationItem.task_id = _task.id;
+
+                        InventoryId = "";
+                        LocationId = "";
+                        Quantity = "";
+
+                    }
+
+                }, p => true));
+
+            }
+        }
+
+
+        private ICommand _deleteItem;
+        public ICommand DeleteItem
+        {
+            get
+            {
+                return _deleteItem ?? (_deleteItem = new RelayCommand((p) =>
+                {
+                    if (SelectedRelocationItem != null)
+                    {
+                        _relocation_items.Remove(SelectedRelocationItem);
+                        refreshRelocationItems();
+                    }
+                }, p => true));
+
+            }
+        }
 
         private string _employeeId;
         public string EmployeeId
@@ -43,8 +101,116 @@ namespace Boxer.ViewModel
                 _remarks = value;
                 onPropertyChanged(nameof(Remarks));
 
-                 _task.remarks = Remarks;
+                _task.remarks = Remarks;
             }
+        }
+
+        private TaskTypes _taskType;
+        public TaskTypes TaskType
+        {
+            get { return _taskType; }
+            set
+            {
+                _taskType = value;
+                onPropertyChanged(nameof(TaskType));
+
+                setVisibility();
+                _task.type = TaskType.ToString();
+            }
+        }
+
+        private void setVisibility()
+        {
+            TypeOrderChosen = false;
+            TypeSupplyChosen = false;
+            TypeRelocationChosen = false;
+            if (TaskType == TaskTypes.order)
+            {
+                TypeOrderChosen = true;
+            }
+            if (TaskType == TaskTypes.supply)
+            {
+                TypeSupplyChosen = true;
+            }
+            if (TaskType == TaskTypes.relocation)
+            {
+                TypeRelocationChosen = true;
+            }
+            onPropertyChanged(nameof(TypeOrderChosen));
+            onPropertyChanged(nameof(TypeSupplyChosen));
+            onPropertyChanged(nameof(TypeRelocationChosen));
+        }
+
+        private string _inventoryId;
+        public string InventoryId
+        {
+            get { return _inventoryId; }
+            set
+            {
+                _inventoryId = value;
+                onPropertyChanged(nameof(InventoryId));
+
+                if (InventoryId != "")
+                    _relocationItem.inventory_id = int.Parse(InventoryId);
+            }
+        }
+
+        private string _locationId;
+        public string LocationId
+        {
+            get { return _locationId; }
+            set
+            {
+                _locationId = value;
+                onPropertyChanged(nameof(LocationId));
+
+                if (LocationId != "")
+                    _relocationItem.location_id = int.Parse(LocationId);
+            }
+        }
+
+        private string _quantity;
+        public string Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _quantity = value;
+                onPropertyChanged(nameof(Quantity));
+
+                if (Quantity != "")
+                    _relocationItem.quantity = int.Parse(Quantity);
+            }
+        }
+
+        private RelocationItem _selectedRelocationItem;
+        public RelocationItem SelectedRelocationItem
+        {
+            get { return _selectedRelocationItem; }
+            set
+            {
+                _selectedRelocationItem = value;
+                onPropertyChanged(nameof(SelectedRelocationItem));
+
+                loadRelocationItem();
+            }
+        }
+        private void loadRelocationItem()
+        {
+            if (SelectedRelocationItem != null)
+            {
+                InventoryId = SelectedRelocationItem.inventory_id.ToString();
+                LocationId = SelectedRelocationItem.location_id.ToString();
+                Quantity = SelectedRelocationItem.quantity.ToString();
+            }
+        }
+
+
+        private void refreshRelocationItems()
+        {
+            relocation_items.Clear();
+            foreach (var item in _relocation_items)
+                relocation_items.Add(item);
         }
 
 
@@ -52,8 +218,20 @@ namespace Boxer.ViewModel
         {
             CancelCommand = new NavigateCommand(navigationService);
             AddTask = new NavigateCommand(navigationService);
+           
+            TypeOrderChosen = true;
 
             _task = new Tasks();
+
+            _relocationItem = new RelocationItem();
+            relocation_items = new BindingList<RelocationItem>();
+            _relocation_items = new List<RelocationItem>();
+
+            employees = new BindingList<Employee>();
+
+            orders = new BindingList<Order>();
+
+            supplies = new BindingList<Supply>();
 
             HeaderText = "Dodaj Zadanie";
             if (task != null){
@@ -62,6 +240,7 @@ namespace Boxer.ViewModel
                 _task = task;
                 EmployeeId = _task.employee_id.ToString();
                 Remarks = _task.remarks;
+                TaskType = (TaskTypes)Enum.Parse(typeof(TaskTypes), _task.type);
             }
         }
     }
